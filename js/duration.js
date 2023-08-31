99dacd05561eed9237b5521d3123bb4caa1fc7db
Easoonn <18336194557@163.com>
2023-08-31 17:34:04 +0800

    Site updated: 2023-08-31 17:34:03

--- js/duration.js
+++ js/duration.js
@@ -1,14 +1,19 @@
-var now = new Date();
-function createtime() {
-    var grt= new Date("03/04/2023 12:00:00");//在此处修改你的建站时间，格式：月/日/年 时:分:秒
-    now.setTime(now.getTime()+250);
-    days = (now - grt ) / 1000 / 60 / 60 / 24; dnum = Math.floor(days);
-    hours = (now - grt ) / 1000 / 60 / 60 - (24 * dnum); hnum = Math.floor(hours);
-    if(String(hnum).length ==1 ){hnum = "0" + hnum;} minutes = (now - grt ) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
-    mnum = Math.floor(minutes); if(String(mnum).length ==1 ){mnum = "0" + mnum;}
-    seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
-    snum = Math.round(seconds); if(String(snum).length ==1 ){snum = "0" + snum;}
-    document.getElementById("timeDate").innerHTML = "本站已安全运行 "+dnum+" 天 ";
-    document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒";
-}
-setInterval("createtime()",250);
+function updateUptime() {
+      var now = new Date();
+      var grt = new Date("03/04/2023 12:00:00");
+      var timeDifference = now - grt;
+
+      var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+      var minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
+      var seconds = Math.floor((timeDifference / 1000) % 60);
+
+      var timeDateElement = document.getElementById("timeDate");
+      var timesElement = document.getElementById("times");
+
+      timeDateElement.textContent = "本站已安全运行 " + days + " 天";
+      timesElement.textContent = hours + " 小时 " + minutes + " 分 " + seconds + " 秒";
+    }
+
+    updateUptime(); // Initial update
+    setInterval(updateUptime, 1000); // Update every second
